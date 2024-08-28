c4ebe7dea59150af1d652946cc0607c188af9da2
mike <loveme@mt.com>
2024-08-29 00:26:40 +0800

    HTTP changes into http

--- server.py
+++ server.py
@@ -2,5 +2,5 @@ from wsgiref.simple_server import make_server
 from hello import application
 
 httpd = make_server('',8080,application)
-print('Serving HTTP on port 8080...')
+print('Serving http on port 8080...')
 httpd.serve_forever()
